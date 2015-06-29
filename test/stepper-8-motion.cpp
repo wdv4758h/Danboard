e31870fc2ecfd5e76b8444f2ab0ce657f3d5ad78
許邱翔 <wdv4758h@gmail.com>
2015-06-29 13:11:31 +0800

    add space after comma

--- test/stepper-8-motion.cpp
+++ test/stepper-8-motion.cpp
@@ -8,28 +8,28 @@ int main()
         return 1;
 
     const stepper step1;
-    const stepper step2(5,6,10,11);
+    const stepper step2(5, 6, 10, 11);
 
     while (1)
     {
-        auto f1 = step1.async_turn(10,60);
-        auto f2 = step2.async_turn(10,30);
+        auto f1 = step1.async_turn(10, 60);
+        auto f2 = step2.async_turn(10, 30);
         f1.get();
         f2.get();
 
-        f2 = step2.async_rturn(10,60);
+        f2 = step2.async_rturn(10, 60);
         f2.get();
 
-        f1 = step1.async_rturn(10,120);
-        f2 = step2.async_turn(10,60);
+        f1 = step1.async_rturn(10, 120);
+        f2 = step2.async_turn(10, 60);
         f1.get();
         f2.get();
 
-        f2 = step2.async_rturn(10,60);
+        f2 = step2.async_rturn(10, 60);
         f2.get();
 
-        f1 = step1.async_turn(10,60);
-        f2 = step2.async_turn(10,30);
+        f1 = step1.async_turn(10, 60);
+        f2 = step2.async_turn(10, 30);
         f1.get();
         f2.get();
     }
